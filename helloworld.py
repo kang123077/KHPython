d0b91a31ef2c9d5bcfd251dfe07ecbec2c4f6ccd
MJ <75807617+kang123077@users.noreply.github.com>
2022-10-12 23:20:53 +0900

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,24 @@
+# 주석
+
+# Hello World를 출력하는 코드
+print('Hello, World!')
+
+a, b = 100, 200
+a, b = b, a + b
+
+print(a , b)
+
+text = 'Hello, World!'
+
+print(text)
+
+# 변수의 이름은 문자, 숫자, 밑줄(_), (!,@ 포함 X)
+# 변수의 이름은 숫자로 시작 X, 공백을 포함할 수 없음
+
+name = "강민준"
+phone_number = "01052022508"
+adress = "서울시 관악구"
+
+print(name)
+print(phone_number)
+print(adress)
